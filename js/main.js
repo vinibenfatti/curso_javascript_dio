66aa989cef11dd88b0d3b8b0ee26dccbf12cd032
unknown <vinibenfatti@gmail.com>
2021-01-26 09:36:01 -0300

    Segundo commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,30 @@
-var nome = "Vinicius Benfatti";
-var n1 = 5;
-var n2 = 3;
-var frase ="Japão é o melhor time do mundo";
+
+var frutas = {nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}
+console.log(frutas.nome);
+lert(frutas[1].nome);
+
+
+//var fruta = {nome:"maça", cor:"vermelha"}
+//console.log(fruta.nome);
+//alert(fruta.cor);
+
+
+//var lista = ["maça","pera","laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//onsole.log(lista.join(" | "));
+
+
+//var nome = "Vinicius Benfatti";
+//var n1 = 5;
+//var n2 = 3;
+//var frase ="Japão é o melhor time do mundo";
 //alert();
 //alert(idade+idade2);
-console.log(nome);
-console.log(n1*n2);
-console.log(frase.replace("Japão", "Brasil"));
-console.log(frase.toUpperCase());
-console.log(frase.toLowerCase());
+//console.log(nome);
+//console.log(n1*n2);
+//console.log(frase.replace("Japão", "Brasil"));
+//console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());

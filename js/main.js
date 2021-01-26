de5df730006d5e9bf2052fffa8c64dc2a24bc59c
unknown <vinibenfatti@gmail.com>
2021-01-26 09:21:21 -0300

    primeiro commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Vinicius Benfatti";
+var n1 = 5;
+var n2 = 3;
+var frase ="Japão é o melhor time do mundo";
+//alert();
+//alert(idade+idade2);
+console.log(nome);
+console.log(n1*n2);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
